cb7259618db0e304a2083c4a7484c8dbe076082c
button onclick="showAlert()"Show alert/button <souvik.das@happay.in>
2024-01-23 12:01:46 +0000

    push

--- pages/Checker Dashboard/jsobjects/session_token_check/session_token_check.js
+++
@@ -1,9 +0,0 @@
-export default {
-SessionActive: () =>  {
-setInterval(async function(){
-	await Fetch_Session_Token.run()
-	if (Fetch_Session_Token.data[0]['result'] == appsmith.store.OPS_TOKEN){//pass
-																				}
-	else {navigateTo('LoginEmail'), showAlert('Session Expired! Please re-login.')}
-}, 120000)
-}}
--- pages/Corporate Details/jsobjects/SessionToken_OnPageLoad/SessionToken_OnPageLoad.js
+++
@@ -1,6 +0,0 @@
-export default {
-SessionActive: async () =>  {
-	if (Fetch_Session_Token.data[0]['result'] == appsmith.store.OPS_TOKEN){ //pass
-																				}
-	else {navigateTo('LoginEmail')}
-}}
